5aaa6e864c735c55489caf33e9a6c893d3e6e9c5
Jiahan Chen <20104729@mail.wit.ie>
2023-11-18 23:19:54 +0000

    e2e error/exception test writeReview.cy.js

--- cypress/e2e/writeReview.cy.js
+++ cypress/e2e/writeReview.cy.js
@@ -24,22 +24,43 @@ describe("Write movie review feature", () => {
             cy.get("button[aria-label='remove from favorites']").next("a[href='/reviews/form']").eq(0).click();
         });
 
-        it('allows user to input and reset a review', () => {
-            cy.get('#author').type('Tester Jiahan');
-            cy.get('#review').type('Jiahan is very handsome');
-            cy.get('li').eq(0).click(); // Selective scoring
-            cy.get('button[type="reset"]').click();
-            cy.get('input[name="author"]').should('have.value', '');
-            cy.get('textarea[name="review"]').should('have.value', '');
-            cy.get('#select-rating').contains('Average');
+        describe("Pass test", () => {
+            it('allows user to input and reset a review', () => {
+                cy.get('#author').type('Tester Jiahan');
+                cy.get('#review').type('Jiahan is very handsome');
+                cy.get('li').eq(0).click(); // Selective scoring
+                cy.get('button[type="reset"]').click();
+                cy.get('input[name="author"]').should('have.value', '');
+                cy.get('textarea[name="review"]').should('have.value', '');
+                cy.get('#select-rating').contains('Average');
+            });
+
+            it('allows user to submit a review', () => {
+                cy.get('#author').type('Tester Jiahan Chen');
+                cy.get('#review').type('Jiahan is very handsome');
+                cy.get('li').eq(0).click(); // 选择评分
+                cy.get('button[type="submit"]').click();
+                cy.contains('Jiahan is very handsome');
+            });
         });
 
-        it('allows user to submit a review', () => {
-            cy.get('#author').type('Tester Jiahan Chen');
-            cy.get('#review').type('Jiahan is very handsome');
-            cy.get('li').eq(0).click(); // 选择评分
-            cy.get('button[type="submit"]').click();
-            cy.contains('Jiahan is very handsome');
+        describe("Error/Exception test", () => {
+            it('None input and submit', () => {
+                cy.get('button[type="submit"]').click();
+                cy.get('.MuiTypography-h6').eq(1).contains("Name is required");
+                cy.get('.MuiTypography-h6').eq(2).contains("Review cannot be empty.");
+            });
+            it('Input name and not input review text', () => {
+                cy.get('#author').type('Tester Jiahan Chen');
+                cy.get('button[type="submit"]').click();
+                cy.get('.MuiTypography-h6').eq(1).contains("Review cannot be empty.");
+            });
+            it('Input name and input review text with little words', () => {
+                cy.get('#author').type('Tester Jiahan Chen');
+                cy.get('#review').type('Jiahan');
+                cy.get('button[type="submit"]').click();
+                cy.get('.MuiTypography-h6').eq(1).contains("Review is too short");
+            });
         });
-    })
+    });
 });
