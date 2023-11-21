b83da1857a75acc47985aabb3c1798b9d9f7888a
Jiahan Chen <3238468729@qq.com>
2023-11-21 00:04:14 +0000

    Optimize movie bundle's size.

--- src/components/movie/movieList/index.js
+++ src/components/movie/movieList/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import Movie from "../movieCard";
+import React, {lazy, Suspense} from "react";
+const Movie = lazy(() => import("../movieCard"));
 import Grid from "@mui/material/Grid";
 
 const MovieList = ({movies, action}) => {
     let movieCards = movies.map((m) => (
         <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
-            <Movie key={m.id} movie={m} action={action}/>
+            <Suspense fallback={<h1>Building movie card</h1>}>
+                <Movie key={m.id} movie={m} action={action}/>
+            </Suspense>
         </Grid>
     ));
     return movieCards;
